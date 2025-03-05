34275b5aace5c8f531488cfef192ed39943f6f0c
ESTUARDO SON <2986622050101@ingenieria.usac.edu.gt>
2025-03-05 13:12:10 -0600

    feat(frontend): dashboard

---
+++ frontend/src/router.tsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard_Admin from "./views/dashboard-admin/Dashboard_Admin";
+import Login from "./views/login/Login";
+
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Login />,
+            errorElement: <div>404</div>
+        },
+        {
+            path: "dashboard-admin",
+            element: <Dashboard_Admin />,
+            children:
+                [
+                    {
+                        path: "",
+                        element: <div>Dashboard</div>,
+                    },
+                    {
+                        path: "tutores",
+                        element: <div>Usuarios</div>,
+                    },
+                    {
+                        path: "horarios",
+                        element: <div>Horarios</div>,
+                    },
+                    {
+                        path: "reportes",
+                        element: <div>Reportes</div>,
+                    }
+                ]
+        }
+    ]
+)
+
+export { router };
--- frontend/src/views/login/Login.tsx
+++ frontend/src/views/login/Login.tsx
@@ -1,18 +1,18 @@
 import './Login.css'
 
-import { Button, Container, Grid2, TextField } from '@mui/material'
+import { Box, Button, Container, Grid2, TextField } from '@mui/material'
 import Item from '../../components/Item'
 
 function Login() {
     return (
         <Grid2 container sx={{ padding: '1rem', height: '100vh' }} spacing={1}>
             <Grid2 size={{ xs: 12, sm: 12, md: 4, lg: 3 }} sx={{ height: '100%' }}>
-                <Item sx={{ position: 'relative', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+                <Item sx={{ position: 'relative', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
                     <Container sx={{ marginBottom: 'auto', maxHeight: '20%' }}>
                         <img id='logo-dtt' src="/src/assets/logo-ecys-fiusac-min.png" alt="Usac" />
                     </Container>
                     <Container sx={{ position: 'absolute', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                        <h3>INICIO DE SESIÓN</h3>
+                        <Box sx={{ typography: 'body1', marginBottom: '1rem' }}>INICIO DE SESIÓN</Box>
                         <TextField
                             required
                             id="usuario"
