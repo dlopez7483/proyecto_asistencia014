ddfd265a71503ecb9ff6b538d399fba67fd55b6f
david <davidlopezxd860@gmail.com>
2025-03-14 02:24:28 -0600

    marcar_entrada

--- Asistencia_013_014.sql
+++ Asistencia_013_014.sql
@@ -1,3 +1,4 @@
+
 DROP DATABASE IF EXISTS Asistencia_013_014;
 CREATE DATABASE IF NOT EXISTS Asistencia_013_014;
 USE Asistencia_013_014;
@@ -25,6 +26,10 @@ CREATE TABLE Horario(
     Hora_salida TIME NOT NULL
 );
 
+
+
+
+
 CREATE TABLE Auxiliar_Horario(
     Id_auxiliar INT NOT NULL,
     Id_horario INT NOT NULL,
@@ -32,6 +37,7 @@ CREATE TABLE Auxiliar_Horario(
     FOREIGN KEY (Id_auxiliar) REFERENCES Auxiliar(Id_auxiliar) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (Id_horario) REFERENCES Horario(Id_horario)
 );
+SELECT * FROM  Auxiliar_Horario;
 
 CREATE TABLE Asistencia_Entrada(
     Id_asistencia INT AUTO_INCREMENT PRIMARY KEY,
@@ -42,6 +48,7 @@ CREATE TABLE Asistencia_Entrada(
     FOREIGN KEY (Id_auxiliar) REFERENCES Auxiliar(Id_auxiliar) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (Id_horario) REFERENCES Horario(Id_horario)
 );
+
 
 CREATE TABLE Asistencia_Salida(
     Id_asistencia INT AUTO_INCREMENT PRIMARY KEY,
@@ -64,3 +71,33 @@ INSERT INTO Configuracion(Periodo_horarios) VALUES('1');
 
 INSERT INTO Auxiliar (Nombre, Apellido, Carne, Telefono, Contrasenia, Id_rol) 
 VALUES ("Bryan", "Vasquez", "201907483", "59453475", "$2a$12$NkhTkuAsmBzlXPME/oRJA.a9insU72zSX.0Kscj/YBmpBT2qZQ59S", 1);
+SELECT * FROM Auxiliar;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE ObtenerHorariosAuxiliarPorRFID(
+    IN p_Codigo_RFID VARCHAR(255), 
+    IN p_DiaSemana VARCHAR(10)
+)
+BEGIN
+    SELECT H.Id_horario, H.Dia_semana, H.Hora_entrada, H.Hora_salida, A.Id_auxiliar
+    FROM Auxiliar A
+    INNER JOIN Auxiliar_Horario AH ON A.Id_auxiliar = AH.Id_auxiliar
+    INNER JOIN Horario H ON AH.Id_horario = H.Id_horario
+    WHERE A.Codigo_RFID = p_Codigo_RFID 
+    AND H.Dia_semana = p_DiaSemana;
+END$$
+
+DELIMITER ;
+
+CALL ObtenerHorariosAuxiliarPorRFID('15F5d', 'Viernes');
+
+
+
+
+SELECT * FROM Asistencia_Entrada;
+delete FROM Asistencia_Entrada WHERE Id_auxiliar=2;
+SELECT * from Horario where Dia_semana="Viernes";
+
+delete from Horario where Dia_semana="Viernes" and Id_horario=6;
