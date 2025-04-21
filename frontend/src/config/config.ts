08aa1b365fe260bd625342633facbe607e3947d5
ESTUARDO SON <2986622050101@ingenieria.usac.edu.gt>
2025-04-21 16:52:14 -0600

    feat(front): Proteccion de vistas

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,5 +1,7 @@
+const { VITE_SERVER_HOST, VITE_SESSION_AUTH } = import.meta.env
+
 export const config = {
-    SERVER_HOST: import.meta.env.VITE_SERVER_HOST,
-    SESSION_AUTH: import.meta.env.VITE_SESSION_AUTH,
+    SERVER_HOST: VITE_SERVER_HOST,
+    SESSION_AUTH: VITE_SESSION_AUTH,
 }
 
